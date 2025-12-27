946820a2b0c9f29aaf10a4bc7b928c26a31f8af7
riptz5 <leandroalvarez@gmail.com>
2025-12-27 02:19:48 -0500

    chore: Remove temporary web/ directory and GCP deployment files

--- web/src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-// UI Components Export
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
-export { Button, buttonVariants, type ButtonProps } from './button'
-export { Badge, badgeVariants, type BadgeProps } from './badge'
