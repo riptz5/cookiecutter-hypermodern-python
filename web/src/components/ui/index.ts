3e653b66c8f7118fcc28838bae4a26c751787dd4
riptz5 <leandroalvarez@gmail.com>
2025-12-27 01:58:24 -0500

    Fix #1: Add foundational UI components (Card, Button, Badge, Toast)

---
+++ web/src/components/ui/index.ts
@@ -0,0 +1,4 @@
+// UI Components Export
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
+export { Button, buttonVariants, type ButtonProps } from './button'
+export { Badge, badgeVariants, type BadgeProps } from './badge'
